f6d133fae1fc2689d44dda8600bb8c33269c0b71
NoahRam33 <merawe481@gmail.com>
2026-06-04 18:09:31 -0500

    Basic Main Structure

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,22 @@
-// #CSS
 import './Main.css'
+import bridge from '../../assets/main__test.jpg'
 
 function Main() {
   return (
-    <main>
-      <h1>Main</h1>
+    <main className="main">
+      <div className="main__container">
+        <img className="main__image" src={bridge} alt="MKE SKYMEDIA Main" />
+
+        <div className="main__image-overlay"></div>
+
+        <div className="main__title main__title--top">
+          <h1>MKE SKYMEDIA</h1>
+        </div>
+        <div className="main__title main__title--bottom">
+        </div>
+      </div>
     </main>
-  );
+  )
 }
 
-export default Main;
+export default Main
