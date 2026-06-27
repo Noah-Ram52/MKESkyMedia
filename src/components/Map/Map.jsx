831621dcc0ca0434051b6b13dc3309c731e6b54c
NoahRam33 <merawe481@gmail.com>
2026-06-26 21:16:11 -0500

    Fixing Map 0.5

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,23 @@
 import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 import { MAP_LOCATION } from '../../utils/location';
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const defaultIcon = L.icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 function Map() {
   const { lat, lon, name } = MAP_LOCATION;
@@ -11,19 +26,20 @@ function Map() {
   return (
     <section className="map">
       <div className="map__toggle">
-    <button
-      className={mapType === 'map' ? 'map__toggle-button active' : 'map__toggle-button'}
-      onClick={() => setMapType('map')}
-    >
-      Map
-    </button>
-    <button
-      className={mapType === 'satellite' ? 'map__toggle-button active' : 'map__toggle-button'}
-      onClick={() => setMapType('satellite')}
-    >
-      Satellite
-    </button>
-  </div>
+        <button
+          className={mapType === 'map' ? 'map__toggle-button active' : 'map__toggle-button'}
+          onClick={() => setMapType('map')}
+        >
+          Map
+        </button>
+        <button
+          className={mapType === 'satellite' ? 'map__toggle-button active' : 'map__toggle-button'}
+          onClick={() => setMapType('satellite')}
+        >
+          Satellite
+        </button>
+      </div>
+
       <MapContainer
         center={[lat, lon]}
         zoom={16}
@@ -38,7 +54,7 @@ function Map() {
               : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           }
         />
-        <Marker position={[lat, lon]}>
+        <Marker position={[lat, lon]} icon={defaultIcon}>
           <Popup>{name}</Popup>
         </Marker>
       </MapContainer>
